61bd43965ad38f2a14748711bc67ca4fe853c5ce
gcho91 <gcho91@gmail.com>
2017-11-24 16:23:42 -0500

    First CW toy prob

---
+++ codewars/CNY.js
@@ -0,0 +1,11 @@
+//convert CNY to USD
+
+//codewars 8kyu https://www.codewars.com/kata/usd-equals-%3E-cny/javascript
+function usdcny(usd){
+var CNY = usd*6.75
+return CNY.toFixed(2)
+}
+
+
+
+usdcny(465)
